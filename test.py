b414bea05d64da30bed32e81eb59d6a0c06cb206
Vincent Lui <61305229+yushinglui@users.noreply.github.com>
2024-08-21 14:28:53 +0000

    test

---
+++ test.py
@@ -0,0 +1,17 @@
+#import libraries
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+import sklearn.datasets
+import plotly.express as px
+from plotly.subplots import make_subplots
+import plotly.graph_objects as go
+import numpy as np
+import plotly.express as px
+import plotly.graph_objects as go
+from sklearn.linear_model import LinearRegression, HuberRegressor
+import warnings
+from IPython.display import display, HTML
+warnings.filterwarnings('ignore')
+display(HTML("<style>.container { width:100% !important; }</style>"))
